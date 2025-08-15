79758c7932275cb4a2a21816cc865ce8177ee44f
katharinefernandes <katharinefv@gmail.com>
2025-08-14 22:07:21 -0300

    add junit test project

---
+++ ProjetoJUNIT/src/Calculadora.java
@@ -0,0 +1,32 @@
+public class Calculadora {
+
+	public double somar(double num1, double num2) {
+		return num1 + num2;
+	}
+
+	public double subtrair(double num1, double num2) {
+		return num1 - num2;
+	}
+
+	public double dividir(double num1, double num2) {
+		double resultado = 0;
+
+		if (num2 != 0) {
+			resultado = num1 / num2;
+		} else {
+			System.out.println("A divisão por zero não é permitida");
+		}
+
+		return resultado;
+
+	}
+
+	public double multiplicar(double num1, double num2) {
+		if (num2 == 0) {
+			return 0;
+		}
+
+		return num1 * num2;
+	}
+
+}
